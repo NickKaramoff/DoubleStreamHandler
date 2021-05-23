9cb766bed46e273160937654e5918e842e64dc12
Nikita Karamov <nick@karamoff.dev>
2021-05-23 12:33:51 +0200

    Rewrite to be more lightweight

--- double_stream_handler/__init__.py
+++ double_stream_handler/__init__.py
@@ -5,49 +5,51 @@ from typing import TextIO, Tuple
 __version__ = "0.1.0"
 
 
-class DoubleStreamHandler(logging.StreamHandler):
+class DoubleStreamHandler(logging.Handler):
     terminator = "\n"
 
     def __init__(
         self,
-        err_from: int = logging.WARNING,
+        err_level: int = logging.WARNING,
         streams: Tuple[TextIO, TextIO] = (sys.stdout, sys.stderr),
     ) -> None:
-        logging.StreamHandler.__init__(self)
+        logging.Handler.__init__(self)
 
-        out_stream, err_stream = streams
-        self.out_handler = logging.StreamHandler(stream=out_stream)
-        self.err_handler = logging.StreamHandler(stream=err_stream)
-        self.err_from = err_from
+        self.out, self.err = streams
+        self.err_level = err_level
 
     def flush(self) -> None:
-        self.err_handler.flush()
-        self.out_handler.flush()
+        self.acquire()
+        try:
+            for stream in [self.err, self.out]:
+                if stream and hasattr(stream, "flush"):
+                    stream.flush()
+        finally:
+            self.release()
 
     def emit(self, record: logging.LogRecord):
-        if record.levelno >= self.err_from:
-            self.err_handler.emit(record=record)
+        if record.levelno >= self.err_level:
+            stream = self.err
         else:
-            self.out_handler.emit(record=record)
+            stream = self.out
 
-    def setStream(self, stream):
-        return None
+        try:
+            msg = self.format(record)
+            stream.write(msg + self.terminator)
+            self.flush()
+        except RecursionError:
+            raise
+        except Exception:
+            self.handleError(record)
 
     def __repr__(self):
-        level = logging.getLevelName(self.level)
-        out_name = getattr(self.out_handler.stream, "name", "")
-        err_name = getattr(self.err_handler.stream, "name", "")
-        #  bpo-36015: name can be an int
-        out_name = str(out_name)
-        err_name = str(err_name)
-        if out_name:
-            out_name += " "
-        if err_name:
-            err_name += " "
+        err_level_name = logging.getLevelName(self.err_level)
+        out_name = str(getattr(self.out, "name", ""))
+        err_name = str(getattr(self.err, "name", ""))
 
-        return "<%s (%s, %s)(%s)>" % (
+        return "<%s err_level='%s', streams=(%s, %s)>" % (
             self.__class__.__name__,
+            err_level_name,
             out_name,
             err_name,
-            level,
         )
